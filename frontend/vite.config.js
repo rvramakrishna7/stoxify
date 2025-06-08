f8603cee8fde8efcee14e6035e16e44ed3a7af60
Krishna <kirankrsna7@outlook.com>
2025-06-09 02:37:45 +0530

    added preview in frontend allowedHosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview:{
+    allowedHosts :['stoxify-f3um.onrender.com'],
+    port:4173,
+  },
   plugins: [react()],
 })
